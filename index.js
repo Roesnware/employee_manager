3aa92adc28cf4b1da0dfcfd32a7e0aa4b3501b75
Roesn <Bubwub04@icloud.com>
2023-10-31 17:53:51 -0400

    switch statemnt logic for input

--- index.js
+++ index.js
@@ -6,26 +6,49 @@ const db = require("./connection.js");
 const actionList = ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"];
 
 // async init func 
-async function init(){
+async function init() {
     await inqChoices();
 }
 
 // async 
 async function inqChoices() {
     // inquirer prompt
-    const { action } =  await inq.prompt([
-            {
-                type: 'list',
-                message: 'What would your like to do?',
-                choices: actionList,
-                name: 'action'
-            }
-        ])
-        // then response
-        // will need to return init if action is not QUIT
-        .then(() => {
-            console.log(action);
-        });
+    const { action } = await inq.prompt([
+        {
+            type: 'list',
+            message: 'What would your like to do?',
+            choices: actionList,
+            name: 'action'
+        }
+    ]);
+    // then response
+    // will need to return init if action is not QUIT
+    console.log(action);
+    switch (action) {
+        case "View All Employees":
+            viewAllEmployees();
+            return init();
+        case "Add Employee":
+            addEmployee();
+            return init();
+        case "Update Employee Role":
+            updateEmployeeRole();
+            return init();
+        case "View All Roles":
+            viewAllRoles();
+            return init();
+        case "Add Role":
+            addRole();
+            return init();
+        case "View All Departments":
+            viewAllDepartments();
+            return init();
+        case "Add Department":
+            addDepartment();
+            return init();
+        default:
+            break;
+    }
 }
 
 // call init 
