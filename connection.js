709adab5099877013e30b8a9b57705a921fff17c
Roesn <Bubwub04@icloud.com>
2023-10-31 19:24:39 -0400

    fixed connection

--- connection.js
+++ connection.js
@@ -1,15 +1,28 @@
+// require packages 
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    },
-    console.log(`Connected to the employee_db database.`)
-);
+// connect to database funct
+async function connect() {
+    // try connecting 
+    try {
+        // connect to db
+        const db = await mysql.createConnection(
+            {
+                host: 'localhost',
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME
+            });
+            // log success
+            console.log(`Connected to the employee_db database.`);
+        // return connection 
+        return db;
+    } catch (err) { // catch err from try connection 
+        console.error(err);
+        throw err;
+    }
+}
 
-module.exports = db;
+// export module 
+module.exports = connect;
