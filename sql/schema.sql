f4e615f7ad713c2e72951cb22fe3fbd2a6d22df3
Roesn <Bubwub04@icloud.com>
2023-10-31 16:51:26 -0400

    role table

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,5 +8,15 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE role (
+  id INT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
 
 
