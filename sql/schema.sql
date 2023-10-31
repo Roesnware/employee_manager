a5b915209c55d1aec9d4b507a423f006a060223e
Roesn <Bubwub04@icloud.com>
2023-10-31 16:51:09 -0400

    departemetn table

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,25 +8,5 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-  id INT NOT NULL PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
-);
 
-CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT, 
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
-  manager_id INT,
-  KEY manager_id(id)
-);
 
