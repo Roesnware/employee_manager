4f38a4d58a30328024187d3a576d67b3e6056dfa
Roesn <Bubwub04@icloud.com>
2023-10-31 16:50:42 -0400

    seed employee table

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -13,3 +13,16 @@ VALUES (001, "Stocker", 35750.00, 001),
        (007, "Accountant", 63500.00, 003),
        (008, "Mortgage Broker", 120000.00, 003);
 
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (001, "John", "Doe", 001),
+       (002, "Trae", "Roy", 002),
+       (003, "Austin", "Hacker", 003),
+       (004, "Matthew", "Shanosky", 004),
+       (005, "Andrew", "McCutcheon", 005),
+       (006, "Naruto", "Uzamaki", 006),
+       (007, "Sasuke", "Uchicha", 002),
+       (008, "Ken", "Kanaki", 004),
+       (009, "Clark", "Kent", 008),
+       (010, "Bruce", "Wayne", 001),
+       (011, "Tony", "Stark", 002);
+
