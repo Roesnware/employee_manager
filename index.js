ce85f1dcebbae19b63e4ed7621754774f71787ea
Roesn <Bubwub04@icloud.com>
2023-10-31 17:02:44 -0400

    basic struc index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // import inquirer package
+const inq = require("inquirer");
 
 // global var
 
+
 // init func 
-
+let init = () => {
     // inquirer prompt
+    inq
+        .prompt([
+            {
 
-    // then response
+            }
+        ])
+        // then response
+        .then((response) => {
+        });
+}
 
 // call init 
+init();
